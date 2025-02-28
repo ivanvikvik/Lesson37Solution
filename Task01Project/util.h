64bbbd20c8363e833e024331b7425ccb87b24765
ivanvikvik <ivanvikvik@gmail.com>
2025-02-28 18:13:05 +0300

    Add project files.

---
+++ Task01Project/util.h
@@ -0,0 +1,6 @@
+#include <iostream>		// string, cout, cin, endl
+#include <string>		// to_string()
+using namespace std;
+
+void init(int* array, int size, int a, int b);
+string convert(int* array, int size);
